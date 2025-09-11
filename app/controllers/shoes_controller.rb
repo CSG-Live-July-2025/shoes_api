c03cb1c98eb096608210e5e6105fe51e57af52b6
Leon Shimizu <lmshimizu@gmail.com>
2025-09-11 21:26:25 +1000

    Implemented all 5 CRUD Actions for the shoes controller and added all 5 routes

--- app/controllers/shoes_controller.rb
+++ app/controllers/shoes_controller.rb
@@ -10,4 +10,34 @@ class ShoesController < ApplicationController
 
     render json: shoe
   end
+
+  def create
+    shoe = Shoe.create(
+      name: params[:name],
+      manufacturer: params[:manufacturer],
+      size: params[:size]
+    )
+
+    render json: shoe
+  end
+
+  def update
+    shoe = Shoe.find(params[:id])
+
+    shoe.update(
+      name: params[:name] || shoe.name,
+      manufacturer: params[:manufacturer] || shoe.manufacturer,
+      size: params[:size] || shoe.size
+    )
+
+    render json: shoe
+  end
+
+  def destroy
+    shoe = Shoe.find(params[:id])
+
+    shoe.destroy
+
+    render json: { message: "Shoe Deleted..." }
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -10,4 +10,7 @@ Rails.application.routes.draw do
 
   get "/shoes" => "shoes#index"
   get "/shoes/:id" => "shoes#show"
+  post "/shoes" => "shoes#create"
+  patch "/shoes/:id" => "shoes#update"
+  delete "/shoes/:id" => "shoes#destroy"
 end
